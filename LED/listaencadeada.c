77e2db56811076dcf51dc3af0098d1005387fd27
Gabriel Iope <gabriel.r.iope@gmail.com>
2023-04-26 16:56:45 -0300

    addnode e removenode

--- LED/listaencadeada.c
+++ LED/listaencadeada.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
 // Definição da estrutura da lista encadeada
 struct node{
@@ -9,13 +8,14 @@ struct node{
 };
 
 // Declaração das funções
-void addnode(int value, struct node** ini);
-void removenode(struct node** node);
-void printlist(struct node** );
+void addnode(int value, struct node** initial);
+void removenode(struct node** initial);
+void freenode(struct node** node);
+void printnode(struct node** initial);
 
 int main(){
     int choice, value;
-    struct node* ini = NULL;
+    struct node* initial = NULL;
     // Inicializa a lista como nula
     
     // Laço do-while que executa até que o usuário decida encerrar o programa
@@ -27,13 +27,13 @@ int main(){
         switch (choice) {
             case 1: 
                 scanf("%d", &value); 
-                addnode(value, &ini); 
+                addnode(value, &initial); 
                 break;
             case 2: 
-                removenode(); 
+                removenode(&initial); 
                 break;
             case 3: 
-                printlist(); 
+                printlist(&initial); 
                 break;
         }
         // Executa a função correspondente à opção escolhida pelo usuário
@@ -43,32 +43,51 @@ int main(){
 }
 
 // Função para adicionar um novo nó na lista
-void addnode(int value, struct node** ini) {
+void addnode(int value, struct node** initial) {
     struct node* newnode = (struct node*) malloc(sizeof(struct node));
+    printf("Nó alocado");
     // Aloca memória para um novo nó
     
     newnode->value = value;
     // Define o valor e o próximo nó como nulo
     
-    if (*ini == NULL) {
+    if (*initial == NULL) {
         newnode->next = newnode;
-        *ini = newnode;
+        *initial = newnode;
         return;
     }
     // Verifica se a lista está vazia, se sim, define o próximo nó como ele mesmo
 
-    newnode->next = (*ini)->next;
-    (*ini)->next = newnode;
-    *ini = newnode;
+    newnode->next = (*initial)->next;
+    (*initial)->next = newnode;
+    *initial = newnode;
     // Adiciona o novo nó ao fim da lista encadeada circular
 }
 
-// Função para remover um nó da lista
-void removenode () {
-   
+
+// Função para remover o nó inicial da lista
+void removenode(struct node** initial) {
+    if (*initial == NULL) {
+        printf("A lista está vazia.\n");
+        return;
+    }
+    // Verifica se a lista está vazia, se sim, retorna uma mensagem de erro
+    
+    struct node* removednode = (*initial)->next;
+    (*initial)->next = removednode->next;
+    // Remove o nó inicial e define o próximo nó como o próximo nó após o nó inicial
+    
+    if (*initial == removednode) {
+        *initial = NULL;
+    }
+    // Se o nó inicial foi o único nó da lista, define o ponteiro inicial como nulo
+    
+    freenode(&removednode);
+    // Desaloca a memória do nó removido
 }
 
-// Função para imprimir a lista encadeada
-void printlist() {
-    
+// Função para desalocar a memória de um nó
+void freenode(struct node** node) {
+    free(*node);
+    *node = NULL;
 }
