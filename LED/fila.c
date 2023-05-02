62f0f4fd8c9f92b5c9f84e4348bc1d8953ac2436
Gabriel Iope <gabriel.r.iope@gmail.com>
2023-05-02 16:37:55 -0300

    Código inicial com necessidade de maior interpretação do enunciado

---
+++ LED/fila.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_SIZE 100
+
+struct elemFila {
+    char info[MAX_SIZE];    
+    struct elemFila * prox;
+};
+
+typedef struct elemFila ElemFila;
+
+struct fila {
+    ElemFila * ini;
+    ElemFila * fim;
+};
+typedef struct fila Fila;
+
+Fila * CriaFila(void);
+int FilaVazia(Fila * f);
+void FilaInsere (Fila * f, char item[MAX_SIZE]);
+int ComparaFilas (Fila* f1, Fila* f2);
+
+int main () {
+    Fila * f1;
+    f1 = fila_cria();
+    printf("Qual item deseja adicionar a FILA 1?");
+    char item[MAX_SIZE];
+    scanf("%s", item);
+    fila_insere(f1, item);
+    Fila * f2;
+    f2 = fila_cria();
+    printf("Qual item deseja adicionar a FILA 2?");
+    scanf("%s", item);
+    fila_insere(f2, item);
+    if (ComparaFilas(f1, f2) == 0)
+        printf("As String são iguais");
+    else
+        printf("As strings são diferentes");
+}
+
+Fila * CriaFila(void)
+{
+    Fila * f = (Fila *)malloc(sizeof(Fila));
+    f->ini = f->fim = NULL;
+    return f;
+}
+
+int FilaVazia(Fila * f)
+{
+    return (f->ini == NULL);
+}
+
+void FilaInsere (Fila * f, char item[MAX_SIZE])
+{
+    ElemFila * n = (ElemFila *)malloc(sizeof(ElemFila));
+    if (n == NULL) 
+    {
+        printf("Sem memoria\n"); 
+        exit(1);
+    }
+    strcpy (item, n->info);
+    n->prox = NULL;
+    if (FilaVazia(f))
+        f->fim->prox = n;
+    else 
+        f->ini = n;
+    f->fim = n;   
+}
+
+int ComparaFilas (Fila* f1, Fila* f2)
+{
+    return strcmp(*f1->item, *f2->item);
+}
