f8355b5e00b3b776559eed8b801a0e11eb345c5f
Gabriel Iope <gabriel.r.iope@gmail.com>
2023-05-02 19:48:24 -0300

    Adição de printf para acompanhamento do código.

--- LED/fila.c
+++ LED/fila.c
@@ -25,13 +25,15 @@ int ComparaFilas (Fila* f1, Fila* f2);
 int main () {
     Fila *f1;
     char item1[MAX_SIZE], item2[MAX_SIZE];
+    printf("\nIniciando a criação da FILA 1...");
     f1 = CriaFila();
-    printf("\nQual item deseja adicionar a FILA 1?");
+    printf("\nQual item deseja adicionar a FILA 1?\n");
     scanf("%s", item1);
     FilaInsere(f1, item1);
     Fila *f2;
+    printf("\nIniciando a criação da FILA 2...");
     f2 = CriaFila();
-    printf("\nQual item deseja adicionar a FILA 2?");
+    printf("\nQual item deseja adicionar a FILA 2?\n");
     scanf("%s", item2);
     FilaInsere(f2, item2);
     if (ComparaFilas(f1, f2) == 0)
@@ -43,27 +45,36 @@ int main () {
 Fila * CriaFila(void)
 {
     Fila *f = (Fila *)malloc(sizeof(Fila));
+    printf("\nFila alocada. ");
     f->inicio = f->fim = NULL;
+    printf("E inicializada.")
     return f;
 }
 
 void FilaInsere (Fila *f, char item[MAX_SIZE])
 {
     No *novo = (No *)malloc(sizeof(No));
+    printf("\nNó alocado para inserção de elemento na fila.");
     if (novo == NULL) 
     {
-        printf("Sem memoria\novo"); 
+        printf("Sem memoria\n"); 
         exit(1);
     }
     strcpy (novo->info, item);
+    printf("\nCopia do valor para o nó.");
     novo->prox = NULL;
     if (!FilaVazia(f))
     {   
         f->fim->prox = novo;
+        printf("Alteração do último nó para inserção de próximo.");
     }
-    f->fim = novo;   
+    f->fim = novo; 
+    printf("\nDefinição do novo nó como o último da fila.");  
     if (f->inicio == NULL)
+    {    
         f->inicio = novo;
+        printf("\nDefinição do nó como nó inicial."); 
+    }
 }
 
 int FilaVazia(Fila *f)
