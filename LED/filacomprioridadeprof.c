c6a697b83146bd3a308104f4b3eb0d11e414ce48
Gabriel Iope <gabriel.r.iope@gmail.com>
2023-05-05 17:19:34 -0300

    upload demonstração em aula fila com prioridade

--- LED/filacomprioridadeprof.c
+++ LED/filacomprioridadeprof.c
@@ -6,7 +6,7 @@ typedef struct {
     char nome[20];
     int chave;
 } Item;
-    
+
 typedef struct {
     Item *v;
     int n, tamanho;
@@ -14,14 +14,19 @@ typedef struct {
 
 typedef FP *p_fp;
 
+void troca(Item *a, Item *b);
+p_fp criar_filaprio(int tam);
+void insere(p_fp fprio, Item item);
+Item extrai_maximo(p_fp fprio);
+void imprime (p_fp fprio);
 
-int main() 
+int main()
 {
     char n[20];
     int prioridade;
     Item novoItem;
     Item excluido;
-    
+
     p_fp f1 = criar_filaprio(5);
     prioridade = 5;
     strcpy (novoItem.nome,"jose");
@@ -33,53 +38,53 @@ int main()
     insere(f1, novoItem);
     imprime(f1);
     excluido = extrai_maximo(f1);
-    printf ("Elemento Maximo: %s \n %d", excluido.nome, excluido.chave);
-    printf("\n apos exclusão\n");
+    printf ("Elemento Maximo: %s\n%d", excluido.nome, excluido.chave);
+    printf("\n\n-----apos exclusao-----\n\n");
     imprime(f1);
 
     return 0;
 }
 
-void troca(Item *a, Item *b) 
+void troca(Item *a, Item *b)
 {
     Item t = *a;
     *a = *b;
     *b = t;
 }
 
-p_fp criar_filaprio(int tam) 
+p_fp criar_filaprio(int tam)
 {
     p_fp fprio = malloc(sizeof(FP));
     fprio->v = malloc(tam * sizeof(Item));
     fprio->n = 0;
     fprio->tamanho = tam;
-    
+
     return fprio;
 }
 
-void insere(p_fp fprio, Item item) 
+void insere(p_fp fprio, Item item)
 {
-    fprio->v[fprio->n] = item;  
+    fprio->v[fprio->n] = item;
     fprio->n++;
 }
 
-Item extrai_maximo(p_fp fprio) 
+Item extrai_maximo(p_fp fprio)
 {
     int j, max = 0;
-    for (j = 1; j < fprio->n; j++) 
+    for (j = 1; j < fprio->n; j++)
     {
         if (fprio->v[max].chave < fprio->v[j].chave)
             max = j;
     }
     troca(&(fprio->v[max]), &(fprio->v[fprio->n-1]));
     fprio->n--;
-    
+
     return fprio->v[fprio->n];
 }
 
 void imprime (p_fp fprio)
 {
     for (int j = 0; j < fprio->n; j++)
-        printf("Elemento %d: \n Nome: %s \n Prioridade: %d", j,
+        printf("Elemento %d: \nNome: %s \nPrioridade: %d\n\n", j,
     fprio->v[j].nome, fprio->v[j].chave);
 }
