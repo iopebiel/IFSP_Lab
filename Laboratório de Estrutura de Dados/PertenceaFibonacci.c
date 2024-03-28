90c9eab8fdcc982345afbfca3d3a6d31d3920c1e
Gabriel Iope <gabriel.r.iope@gmail.com>
2024-03-28 16:17:16 -0300

    update files

---
+++ Laboratório de Estrutura de Dados/PertenceaFibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    int a, b, auxiliar, i, n;
+
+    a = 0;
+    b = 1;
+
+    printf("Digite um número: ");
+    scanf("%d", &n);
+    
+    printf("Sequência Fibbonaci:\n");     
+    while (auxiliar < n) 
+    {
+        auxiliar = a + b;
+        a = b;
+        b = auxiliar;
+        
+        printf("%d\n", auxiliar);
+    }
+    
+    if (auxiliar == n)
+        printf("O número pertence a sequência de Fibonacci!");
+    else
+        printf("O número não pertence a sequência de Fibonacci!");
+    
+}
